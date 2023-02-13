9e55b2e7ab4dd55766d36ccd0e9583c9bb5d4b68
vista <SIRTAY09@GMAIL.COM>
2023-02-13 07:07:22 -0500

    Committing all

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vista
+ *
+ */
+module first_project {
+}
---
+++ src/org/paumard/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.paumard;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+    }
+
+}
